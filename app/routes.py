711d404009609c0974ac90a3f164c723d280970d
Grace Meng <gracemeng93@gmail.com>
2022-05-05 17:43:33 +0800

    cosmetic base.html

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,11 @@
 from turtle import title
 from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
-from flask_login import current_user, login_user
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
-from flask_login import logout_user
-from flask_login import login_required
 from flask import request
 from werkzeug.urls import url_parse
-from app import db
-from app.forms import RegistrationForm
 
 
 @app.route('/')
@@ -63,5 +59,5 @@ def game():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)
+    return render_template('user.html', user=user, title="Profile")
 
